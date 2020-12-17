9ab34fd08062c0ba65ae0316dacf28a6d2782514
Yudha13Koley <yudhajit1998@gmail.com>
2020-12-17 13:54:56 +0530

    Added : axios call is made to json server for adding employee in the json file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-          <Route path=''>
+          <Route exact path="/payroll-form">
             <PayrollForm />
           </Route>
         </Switch>
